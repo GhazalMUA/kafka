efa4126aacfca7da3f569ad0b8ba2ad07398f321
Ghazal Hafezi <hafezibirganighazal@gmail.com>
2026-09-08 17:25:52 +0330

    change models\init file

--- marine_telemetry_platform/shared/database/models/__init__.py
+++ marine_telemetry_platform/shared/database/models/__init__.py
@@ -1,3 +1,12 @@
-from shared.database.models.telemetry_measurement import TelemetryMeasurement
 
-__all__ = ["TelemetryMeasurement"]
+from shared.database.models.equipment import Equipment
+from shared.database.models.sensor import Sensor
+from shared.database.models.telemetry_measurement import TelemetryMeasurement
+from shared.database.models.vessel import Vessel
+
+__all__ = [
+    "Equipment",
+    "Sensor",
+    "TelemetryMeasurement",
+    "Vessel",
+]
